9d7d2a4a19a7ff635defa73e1658c696c2059990
Alessandro Solbiati <lessandro@fb.com>
2022-04-17 11:29:52 +0100

    [contract] unique hashes

--- contracts/HelloWorldNFT.sol
+++ contracts/HelloWorldNFT.sol
@@ -4,6 +4,26 @@ import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/utils/Counters.sol"
 
 contract HelloWorldNFT is ERC721 {
+	using Counters for Counters.Counter;
+	Counters.Counter private _tokenIDs;
+	mapping(string => uint8) hashes;
+
+
 	constructor() public ERC721("HelloWorldNFT", "hwNFT");
 
+	function awardItem(address recipient, string memory hash, string memory metadata)
+		public
+		returns (uint256)
+
+	{
+		require(hashes[hash] != 1);
+		hashes[hash] = 1;
+		_tokenIDs.increment();
+		uint256 newItemId = _tokenIDs.current();
+
+		_mint(recipient, newItemId);
+		_setTokenURI(newItemId, metadata);
+
+		return newItemId;
+	}
 }
